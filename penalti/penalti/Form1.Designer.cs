4b2a056257a5f2cfcd86face1d9943569227d486
Muhammed Salih KOÇ <muhammed.salih.koc@gmail.com>
2021-08-14 10:17:12 +0300

    Penaltı Oyunu
    Penaltı Oyunu

---
+++ penalti/penalti/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace penalti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerKaleci = new System.Windows.Forms.Timer(this.components);
+            this.timerSut = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timerKaleci
+            // 
+            this.timerKaleci.Enabled = true;
+            this.timerKaleci.Interval = 50;
+            this.timerKaleci.Tick += new System.EventHandler(this.timerKaleci_Tick);
+            // 
+            // timerSut
+            // 
+            this.timerSut.Interval = 10;
+            this.timerSut.Tick += new System.EventHandler(this.timerSut_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(457, 298);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(102, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tekrar Şut Çek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 353);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Penaltı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerKaleci;
+        private System.Windows.Forms.Timer timerSut;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ penalti/penalti/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace penalti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        Graphics g;
+        Pen kalem_kale = new Pen(Color.Black, 5);
+        Pen kalem_kaleci = new Pen(Color.Blue, 5);
+        Brush firca_top = new SolidBrush(Color.Red);
+
+        bool sol = true;
+        int kaleci_x = 270, kaleci_y = 35, topx = 275, topy = 235;
+
+        private void timerSut_Tick(object sender, EventArgs e)
+        {
+            Brush firca_top_sil = new SolidBrush(this.BackColor);
+            g.FillEllipse(firca_top_sil, topx, topy, 10, 10);
+
+            if (Math.Abs(kaleci_x - topx) <= 20 && Math.Abs(kaleci_y - topy) <= 10)
+            {
+                timerKaleci.Enabled = false;
+                timerSut.Enabled = false;
+                button1.Enabled = true;
+
+                MessageBox.Show("KALECİ DUTTU ;)");
+            }
+            else if (topy < kaleci_y)
+            {
+                timerKaleci.Enabled = false;
+                timerSut.Enabled = false;
+                button1.Enabled = true;
+
+                MessageBox.Show("GOOOOOLLLLLLLLLLLL!!!!!!");
+            }
+          else
+            topy = topy - 5;
+
+
+            g.FillEllipse(firca_top, topx, topy, 10, 10);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen kalem_kaleci_sil = new Pen(this.BackColor, 5);
+            g.DrawRectangle(kalem_kaleci_sil, kaleci_x, kaleci_y, 20, 5);
+            Brush firca_top_sil = new SolidBrush(this.BackColor);            
+            g.FillEllipse(firca_top_sil, topx, topy, 10, 10);
+            sol = true;
+            kaleci_x = 270;
+            kaleci_y = 35;
+            topx = 275;
+            topy = 235;
+
+            g.DrawRectangle(kalem_kaleci, kaleci_x, kaleci_y, 20, 5);
+            g.FillEllipse(firca_top, topx, topy, 10, 10);
+
+            timerKaleci.Enabled = true;
+            timerSut.Enabled = false;
+            button1.Enabled = false;
+            
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+                timerSut.Enabled = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            g = this.CreateGraphics();
+            g.DrawRectangle(kalem_kale, 200, 10, 5, 20); // Sol Direk
+            g.DrawRectangle(kalem_kale, 210, 10, 150, 5); // Üst Direk
+            g.DrawRectangle(kalem_kale, 360, 10, 5, 20); // Sağ Direk
+            g.DrawRectangle(kalem_kaleci, kaleci_x, kaleci_y, 20,5); //Kaleci
+            g.FillEllipse(firca_top, topx, topy, 10, 10); //Top
+
+
+
+        }
+
+        private void timerKaleci_Tick(object sender, EventArgs e)
+        {
+            Pen kalem_kaleci_sil = new Pen(this.BackColor, 5);
+            Brush firca_top_sil = new SolidBrush(this.BackColor);
+
+            g.DrawRectangle(kalem_kaleci_sil, kaleci_x, kaleci_y, 20, 5);
+            g.FillEllipse(firca_top_sil, topx, topy, 10, 10);
+           
+
+            if(timerSut.Enabled==false)
+            { 
+
+            Random r = new Random();
+            topx = r.Next(210, 345);
+
+            }
+            if (sol == true)
+                kaleci_x = kaleci_x - 5;
+            else
+                kaleci_x = kaleci_x + 5;
+
+            if (kaleci_x <= 210) sol = false;
+            if (kaleci_x >= 335) sol = true;
+
+            g.DrawRectangle(kalem_kaleci, kaleci_x, kaleci_y, 20, 5);
+            g.FillEllipse(firca_top, topx, topy, 10, 10);
+        }
+    }
+}
